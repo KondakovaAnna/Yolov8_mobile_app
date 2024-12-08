0bd15d07e26bb0cc6a48c22ed8e89096e316a191
selfmadekirich <selfmadekirich@gmail.com>
2024-12-08 23:14:38 +0300

    service without docker ready

---
+++ bentoml/BentoYolo/service.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import json
+import os
+import typing as t
+from typing import BinaryIO
+from pathlib import Path
+import PIL
+from PIL.Image import Image as PILImage
+import bentoml
+from bentoml.validators import ContentType
+from pydantic import BaseModel
+import numpy as np
+
+"""
+class Params(BaseModel):
+    images: PILImage
+    arbitrary_types_allowed=True
+"""
+
+Image = t.Annotated[Path, ContentType("image/*")]
+
+
+@bentoml.service(resources={"gpu": 1})
+class YoloV8:
+    def __init__(self):
+        from ultralytics import YOLO
+
+        yolo_model = os.getenv("YOLO_MODEL", "../best.pt")
+
+        self.model = YOLO(yolo_model)
+
+    @bentoml.api()
+    def predict(self, images: PILImage) -> list[dict]:
+        print(images)
+        results = self.model.predict(images)[0]
+        return json.loads(results.tojson())
+        #return [json.loads(result.tojson()) for result in results]
+
+    @bentoml.api
+    def render(self, images: PILImage) -> PILImage:
+        result = self.model.predict(images)[0]
+        output = "aaaaaa.jpeg" #images.parent.joinpath(f"{images.stem}_result{images.suffix}")
+        result.save(output)
+        return Image.open(output)
---
+++ front/main.py
@@ -0,0 +1,82 @@
+import streamlit as st
+from io import StringIO
+import requests
+import os
+from PIL import Image
+from io import BytesIO
+import cv2
+
+BENTO_HOST = os.getenv("BENTO_HOST", "localhost")
+BENTO_PORT = os.getenv("BENTO_PORT", "3000")
+
+def highlight_borders(img: str, info: dict):
+    boxes = [x["box"] for x in info]    # Координаты прямоугольника вокруг найденного объекта
+    probs = [x["confidence"] for x in info]                          # Уверенность сети в данной детекции
+    cls = [x["class"] for x in info]            # Индексы имён классов
+    names = [x["name"] for x in info]            # Конвертация индексов в название классов
+
+    image = cv2.imread(img, cv2.IMREAD_COLOR)  # Открытие цветного изображения (примечание: открывается в BGR)
+
+    detection_visualization = image.copy()
+
+    text_font = cv2.FONT_HERSHEY_DUPLEX
+    text_scale = 2.0
+    colors = [(50, 20, 200), (200, 20, 100)]
+    text_thickness = 5
+
+    for detecion_id in range(len(boxes)):
+        target_object_name = names[detecion_id]
+        tob = boxes[detecion_id]  # XYXY format
+        target_object_prob = float(probs[detecion_id])
+
+        detection_visualization = cv2.rectangle(
+          detection_visualization,
+            tuple([int(tob["x1"]), int(tob["y1"])]),
+            tuple([int(tob["x2"]), int(tob["y2"])]),
+            colors[detecion_id % len(colors)],
+            5
+        )
+    
+    for detecion_id in range(len(boxes)):
+        target_object_name = names[detecion_id]
+        tob = boxes[detecion_id]
+        target_object_prob = float(probs[detecion_id])
+
+        input_text = '{} {:.2f}'.format(target_object_name, target_object_prob)
+
+        (text_width, text_height), baseline = cv2.getTextSize(input_text, text_font, text_scale, text_thickness)
+
+        detection_visualization = cv2.putText(
+            detection_visualization,
+            input_text,
+            (int(tob["x1"]) + 10, int(tob["y1"]) + text_height + baseline),
+            text_font,
+            text_scale,
+            colors[detecion_id % len(colors)],
+            text_thickness
+        )
+    
+    cv2.imwrite(f"gen_{img}", detection_visualization)
+
+
+
+
+def post_img(route: str,img):
+    r = requests.post(
+        url=f"http://{BENTO_HOST}:{BENTO_PORT}{route}",
+        files={"images": img}
+    )
+
+    if r.status_code not in [200,201]:
+        raise Exception("blyat")
+    return r
+
+uploaded_file = st.file_uploader("Choose a file", type=["png","jpg","jpeg"])
+if uploaded_file is not None:
+    with open(uploaded_file.name,"wb") as f: 
+        f.write(uploaded_file.getbuffer())         
+    result = post_img("/predict", open(uploaded_file.name, 'rb'))
+    #st.write(result.json())
+    highlight_borders(uploaded_file.name, result.json())
+    st.image(Image.open(f"gen_{uploaded_file.name}"))
+    #st.image(uploaded_file)
